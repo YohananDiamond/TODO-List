69073d42b8b4f19f67e05d0f05bbfe894949edb3
Italo Seara <54297273+italoseara@users.noreply.github.com>
2020-10-30 11:38:43 -0300

    Local Storage

--- script.js
+++ script.js
@@ -1,11 +1,30 @@
+if (localStorage.TODOS == undefined) localStorage.TODOS = '';
+
+document.querySelector('#TODOS').innerHTML += localStorage.TODOS;
 document.querySelector('.btn-add').addEventListener('click', add);
-document.querySelector('body').addEventListener('keypress', (e) => {if (e.key === 'Enter') add()});
+document.querySelector('body').addEventListener('keypress', (e) => { if (e.key === 'Enter') add() });
+update();
+
+document.querySelectorAll('.check').forEach(item => {
+    $(item).change(function () {
+        if ($(this).is(":checked")) {
+            $(closest(item, 'div')).addClass('done');
+        } else {
+            $(closest(item, 'div')).removeClass('done');
+        }
+        localStorage.TODOS = document.querySelector('#TODOS').innerHTML.slice(237)
+    });
+});
+
+document.querySelectorAll('.done input').forEach(item => {
+    item.checked = true;
+})
 
 function closest(el, selector) {
     var matchesFn;
 
     // find vendor prefix
-    ['matches','webkitMatchesSelector','mozMatchesSelector','msMatchesSelector','oMatchesSelector'].some(function(fn) {
+    ['matches', 'webkitMatchesSelector', 'mozMatchesSelector', 'msMatchesSelector', 'oMatchesSelector'].some(function (fn) {
         if (typeof document.body[fn] == 'function') {
             matchesFn = fn;
             return true;
@@ -37,12 +56,21 @@ function add() {
     clone.querySelector('h2').textContent = document.querySelector('.inp-task').value;
     clone.style.display = 'block';
 
-    document.querySelector('#TODOS').appendChild(clone);
+    document.querySelector('#TODOS').innerHTML += clone.outerHTML;
 
     document.querySelector('.inp-task').value = '';
 
-    clone.querySelector('.btn-remove').addEventListener('click', () => {
-        closest(clone.querySelector('.btn-remove'), 'div').remove();
-    })
+    update();
 
+    localStorage.TODOS = document.querySelector('#TODOS').innerHTML.slice(237);
+}
+
+function update() {
+    document.querySelectorAll('.todo').forEach(item => {
+        item.querySelector('.btn-remove').addEventListener('click', () => {
+            closest(item.querySelector('.btn-remove'), 'div').remove();
+
+            localStorage.TODOS = document.querySelector('#TODOS').innerHTML.slice(237);
+        });
+    })
 }
